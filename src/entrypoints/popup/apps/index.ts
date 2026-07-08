e225c35119a6a4181cb51b0fd530551c34e53da8
qiancheng.zhao <qiancheng.zhao@alo7.com>
2026-07-08 15:09:34 +0800

    ✨feat: 移除今日头条发布器相关文件 <🐥 2026-07-08 15:09:32>

--- src/entrypoints/popup/apps/index.ts
+++ src/entrypoints/popup/apps/index.ts
@@ -2,10 +2,9 @@ import type { PopupApp } from './types';
 import tableExport from './table-export';
 import qImageHelper from './q-image-helper';
 import wechatPublisher from './wechat-publisher';
-import toutiaoPublisher from './toutiao-publisher';
 
 /**
  * All registered popup sub-apps.
  * Each id must correspond to an entry in src/features/registry.ts.
  */
-export const apps: PopupApp[] = [tableExport, qImageHelper, wechatPublisher, toutiaoPublisher];
+export const apps: PopupApp[] = [tableExport, qImageHelper, wechatPublisher];
